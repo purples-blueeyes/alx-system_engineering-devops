d1bc5d468fa1d58cdd92af479479f2cc77708d99
Anthoine <dzimahanthoine@gmail.com>
2023-05-29 15:18:55 +0000

    Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message.

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
 # To make it possible to login with the holberton user and open a file without any error message.
 
-exec {'replace-1':
-  provider => shell,
-  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
-  before   => Exec['replace-2'],
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
-exec {'replace-2':
-  provider => shell, 
-  command => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+# Increase soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
