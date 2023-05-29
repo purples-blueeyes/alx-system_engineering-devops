6be8c2f7026c36f9e712ad4843116b038fd11ee2
Anthoine <dzimahanthoine@gmail.com>
2023-05-29 11:26:12 +0000

    Debugging_3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# a puppet manuscript to GET HTTP method is requested to Apache web server to Fix 500 error
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+sed -1 's/phpp/php/g' /var/www/html/wp-settings.php
